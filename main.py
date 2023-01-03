b97704498f8627699314169c003667788faff89e
David Schenck <david.schenck@outlook.com>
2023-01-03 19:49:26 +0100

    new deploy

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import streamlit_highcharts as sh
+import easychart
+
+import time
+
+
+@st.cache
+def retrieve_data(i):
+    time.sleep(5)
+    return [3 + 4 * x + x ** 2 for x in range(int(i))]
+
 
 st.title("Hello world!")
 
-x = st.slider("Select value", key="x")
+i = st.number_input("Enter x")
 
-st.write(x, "squared is", x * x)
+
+chart = easychart.new()
+chart.plot(retrieve_data(i))
+
+
+sh.streamlit_highcharts(chart.serialize())
 
